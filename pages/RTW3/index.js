a4d80b77c4ead2bbcef6ae23331d3b52786e08b3
artlu99 <@artlu99>
2022-07-26 23:40:19 +0800

    buttons

--- pages/RTW3/index.js
+++ pages/RTW3/index.js
@@ -78,31 +78,44 @@ export default function Home() {
     if (connected) {
         if (verifiedCredential) {
             return (
-                <div className="flex min-h-screen">
+                <div className="flex min-h-screen justify-center">
                     <div className="p-8 text-center">
                         <div className="font-bold"><h1>Welcome, fren!</h1></div>
-                        <button onClick={disconnect}>Disconnect</button>
+                        <div className="p-8">
+                            <button type="button" onClick={disconnect} className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+                                Disconnect
+                            </button>
+                        </div>
                     </div>
                 </div>
             )
         } else {
             return (
-                <div className="flex min-h-screen">
+                <div className="flex min-h-screen justify-center">
                     <div className="p-8 text-center">
                         <div className="font-bold"><h1>Gm, please sign with a wallet which holds a Week 3 PoK.</h1></div>
-                        <button onClick={disconnect}>Disconnect</button>
-                    </div>
-                </div>
+                        <div className="p-8">
+                            <button type="button" onClick={disconnect} className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+                                Disconnect
+                            </button>
+                        </div>
+                    </div >
+                </div >
 
             )
         }
     } else {
         return (
-            <div className="flex min-h-screen">
+            <div className="flex min-h-screen justify-center">
                 <div className="p-8 text-center">
                     <button className="hidden" onClick={saveSigningCondition}>Save Signing Condition|</button>
-                    <button onClick={retrieveJWT}>Sign in</button>
                     <div className="text-xs">(please use a wallet that holds a MintKudos🎉 Alchemy Road To Web3 Week3 PoK)</div>
+                    <div className="p-8">
+                        <button type="button" onClick={retrieveJWT} className="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 mr-2 mb-2">
+                            <svg class="w-4 h-4 mr-2 -ml-1 text-[#626890]" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="ethereum" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
+                            Sign in
+                        </button>
+                    </div>
                 </div>
             </div>
         )
