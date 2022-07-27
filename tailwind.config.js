b6ce1557fe7502f912a03263a2fe9543fd02d4f9
artlu99 <@artlu99>
2022-07-27 15:40:14 +0800

    the Elon Musk problem (one of them)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+defaultTheme = require('tailwindcss/defaultTheme');
+fontFamily = defaultTheme.fontFamily;
+fontFamily.sans = [
+  'Tahoma',
+  'system-ui'
+  // <-- you may provide more font fallbacks here
+];
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
-  darkMode: "class", 
+  darkMode: "class",
   theme: {
     extend: {},
+    fontFamily: fontFamily
   },
   plugins: [],
 }
